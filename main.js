c01657661c6818cb54ccf4a9ca3d0d12aef71460
Timothy Tomas <timothyjtee@gmail.com>
2022-04-10 18:49:42 -0700

    Implement addBookToList functionality

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 let myLibrary = [];
-const inputValue = document.querySelector('#input-book')
+const input = document.querySelector('.two > input')
+let inputValue = '';
+let count = 0
+
+const submit = document.querySelector('.submit');
+submit.addEventListener('click', addBookToList);
+
+const display = document.querySelector('.display');
+display.addEventListener('click', displayBookList);
 
 function Book() {
   // the constructor...
@@ -8,3 +16,21 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+function addBookToList() {
+  let book = {};
+  inputValue = input.value;
+  book.author = inputValue;
+  myLibrary.push(book);
+  console.log(myLibrary);
+  count++;
+}
+
+function displayBookList() {
+  const authors = myLibrary.map(book => {
+    console.log(book['author']);
+  });
+  return authors;
+}
+
+// Modal Behavior
