6408e5f029bb022efa11f2e2be495e54ecae5fe9
Timothy Tomas <timothyjtee@gmail.com>
2022-04-11 00:34:15 -0700

    Create new book card in DOM

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 let myLibrary = [];
-const input = document.querySelector('.two > input')
-let inputValue = '';
-let count = 0
 
-const submit = document.querySelector('.submit');
-submit.addEventListener('click', addBookToList);
-
-const display = document.querySelector('.display');
-display.addEventListener('click', displayBookList);
-
-function Book() {
-  // the constructor...
+function Book(author,title,pages,read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-function addBookToList() {
-  let book = {};
-  inputValue = input.value;
-  book.author = inputValue;
-  myLibrary.push(book);
-  console.log(myLibrary);
-  count++;
-}
-
-function displayBookList() {
-  const authors = myLibrary.map(book => {
-    console.log(book['author']);
-  });
-  return authors;
-}
-
-// Modal Behavior
+//============ Modal Behavior ================
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
@@ -70,3 +44,61 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active');
 }
+
+//========== Submit New Book ============
+const submitBook = document.querySelector('.submit-new-book');
+submitBook.addEventListener('click', addBookToLibrary);
+
+// Reading List container
+const readingList = document.querySelector('.reading-list');
+function addBookToLibrary() {
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let read = document.getElementById('read').checked;
+
+  const book = new Book(author, title, pages, read);
+  
+  myLibrary.push(book);
+  // Create new book card
+  
+  const container = document.createElement('div');
+  container.className = 'card';
+
+  const cardImg = document.createElement('div');
+  cardImg.className = 'card-img';
+  container.appendChild(cardImg);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.className = 'card-info';
+  container.appendChild(cardInfo);
+
+  const textTitle = document.createElement('p');
+  textTitle.className = 'text-title';
+  textTitle.innerText = book['title'];
+  cardInfo.appendChild(textTitle);
+
+  const textBody = document.createElement('p');
+  textBody.className = 'text-body';
+  textBody.innerText = `${book['author']} ${book['pages']}${book['read']}`;
+  cardInfo.appendChild(textBody);
+
+  const cardBtn = document.createElement('button');
+  cardBtn.className = 'card-button';
+  cardBtn.innerText = 'Read More';
+  cardInfo.appendChild(cardBtn);
+
+  readingList.appendChild(container);
+  console.log(myLibrary);
+}
+
+//============= Display Book List =================
+// DOM structure
+/* <div class="card"> check
+    <div class="card-img"></div> check
+    <div class="card-info"> check
+      <p class="text-title">Card title</p> check
+      <p class="text-body">Lorem Ipsum dolor sit amet</p> check
+      <button class="card-button">Read More</button> check
+    </div>
+  </div> */
